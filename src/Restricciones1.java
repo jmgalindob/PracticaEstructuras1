f261f56e1a7e7f648a2961abc0ee91c6cdad1eb7
jmgalindob <60240889+jmgalindob@users.noreply.github.com>
2020-05-30 21:16:14 -0500

    Add files via upload

--- src/Restricciones1.java
+++ src/Restricciones1.java
@@ -13,41 +13,56 @@ public class Restricciones1
     LinkedList<mat20192> materia;
     LinkedList<Aulas> aula;
     LinkedList<DistanciasBloques> distb;
-    
+
     public void retriccionProfesor()
     {
         ArregloPa20192 refPa = new ArregloPa20192();
-        
+
         refPa.leerpa();
         planAcademico = refPa.pa;
-        
+
         int tpa = planAcademico.size();
         int c = 0;
         int o = 0;
-        
-        
-        
+
         String iguales = null;
         String m = null;
-        for(int i =0;i < tpa -1;i++)
+        for(int i =0;i < tpa - 1 ;i++)
         {
-            
-                 if(planAcademico.get(i).getProfesor() == 1 && planAcademico.get(i).getProfesor() != planAcademico.get(i+1).getProfesor())
+
+            if(planAcademico.get(i).getProfesor() == 1)
+            {
+                continue;
+                //System.out.println( planAcademico.get(i).getMateria() + " no tiene profesor asignado");
+            }
+
+            if(planAcademico.get(i).getProfesor() != 1 && planAcademico.get(i).getProfesor() == planAcademico.get(i+1).getProfesor())
+            {
+
+                for(int j = i + 1; j < tpa ; j++)
                 {
-                    for(int j = 1;j < tpa;j++)
-                    {   
-                    if(planAcademico.get(i).getDia().compareTo(planAcademico.get(j).getDia()) == 1)
+                    if(planAcademico.get(i).getDia().equalsIgnoreCase(planAcademico.get(j).getDia()) )
                     {
-                    c++;
-                    iguales = planAcademico.get(j).getMateria();
-                    m= planAcademico.get(j).getDia();
-                    System.out.println(iguales + "   " + m);
+                        //System.out.println(planAcademico.get(i).getDia() + "   "+ planAcademico.get(j).getDia());
+                        //break;
+
+                        if(planAcademico.get(i).getInicio().equalsIgnoreCase( planAcademico.get(i + 1).getInicio()))
+                        {
+                            planAcademico.get(j).setInicio("00:00") ;
+                            planAcademico.get(j).setFin("00:00");
+                        } 
+
                     }
                 }
+
             }
+            
         }
-        System.out.println(c);
-        
-        
+        //LinkedList<pa20192>
+        for(int i = 0; i < tpa; i++)
+        {
+            System.out.println(planAcademico.get(i).getInicio());
+        }
+        System.out.println(c/2);
     }
 }
